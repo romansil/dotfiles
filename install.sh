52dae13b88f713be63a81f1b3abc988a4ee06ffd
rs <rs@hack.more.systems>
2017-07-22 11:06:08 +0200

    some changes

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/bashrc $HOME/.bashrc
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+
+# TODO check if already here and git installed
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
