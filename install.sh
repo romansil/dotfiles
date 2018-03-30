cf277919b421e4bab8c9885845fa9d46086b9d45
Roman Silberschneider <roman.silberschneider@iaik.tugraz.at>
2018-03-30 10:33:17 +0200

    old setup scripts removed

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -s $(pwd)/vimrc $HOME/.vimrc
-ln -s $(pwd)/bashrc $HOME/.bashrc
-ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
-
-# TODO check if already here and git installed
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
